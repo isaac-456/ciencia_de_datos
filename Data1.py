3f3c5a45d94544ac973e032b30fdbd35fe476352
isaac-456 <isaacbeca124@gmail.com>
2026-09-23 18:27:46 +0000

    add Data1.py

--- Data1.py
+++ Data1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as st
-name_link = ""
+name_link = "https://raw.githubusercontent.com/adsofito/ciencia-datos"
 names_data=pd.read_csv(names link)
 
 st.title("streamlit and pandas")
